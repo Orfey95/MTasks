2e4f9a9d75fa79b9c1ccd25fe576a84a262a8f0c
Sasha <sasha7692@gmail.com>
2020-04-30 16:08:54 +0300

    Create task2.sh

---
+++ Task1/task2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+set -e
+
+username=$1
+commands=$2
+
+# Convert command string to command array
+commands_array=($(echo $commands | tr " " "\n"))
+
+# Get aaray os full path of commands
+full_path_commands_array=()
+for i in "${commands_array[@]}"
+do
+	if [ -n "$(which $i)" ]; then
+		full_path_commands_array+=( $(which $i) )
+	fi
+done
+
+# Convert command string to command array
+full_path_commands_string=$(IFS=','; echo "${full_path_commands_array[*]}")
+
+# Add user to sudo group
+usermod -aG sudo $username
+
+# Create sudoers file for user
+touch /etc/sudoers.d/$username
+
+# Deny ALL sudo command
+echo "$username  ALL=(ALL) !ALL $full_path_commands_string" > /etc/sudoers.d/$username
