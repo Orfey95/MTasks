cb01e13c7dc0142db00661f5d3421313178ef3b9
Sasha <sasha7692@gmail.com>
2020-05-05 14:50:38 +0300

    Update mvnset.sh

--- Task1/mvnset.sh
+++ Task1/mvnset.sh
@@ -1,18 +1,40 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 
 # Variables
 mvn_version=$1 # Example: 3.6.1
+VAR_ERROR=1
+REJECT_INSTALLATION=2
+NOT_SUDO=3
+ANSWER_ERROR=4
+
+# Check parameters
+if [ $# != 1 ]; then
+	echo "You forgot to enter maven version"
+	exit $VAR_ERROR
+fi
+
+# Check sudo or root
+if [ "$EUID" -ne 0 ]
+  then echo "This command requires SUDO";
+  exit $NOT_SUDO;
+fi
 
 # Сhecking if the required version is installed
-if ! ls /opt | grep apache-maven-$mvn_version > /dev/null; then
-	wget -qO- https://archive.apache.org/dist/maven/maven-3/$mvn_version/binaries/apache-maven-$mvn_version-bin.tar.gz -P /opt | tar -xvz -C /opt > /dev/null
+if [ ! -d /opt/apache-maven-"$mvn_version" ]; then	
+	read -p "The version of Maven you requested is not installed. Install? Continue (Y/n)? " -e choice
+	case "$choice" in 
+	  y|Y|"" ) wget -qO- https://archive.apache.org/dist/maven/maven-3/"$mvn_version"/binaries/apache-maven-"$mvn_version"-bin.tar.gz -P /opt | tar -xvz -C /opt > /dev/null 2>&1;;
+	  n|N ) exit $REJECT_INSTALLATION;;
+	  * ) echo "Incorrect answer"; exit $ANSWER_ERROR;;
+	esac
 fi
 
-# Add required version to $PATH
-if ! cat $HOME/.profile | grep apache-maven; then
-	echo "export PATH=/opt/apache-maven-$mvn_version/bin:$PATH" | tee -a $HOME/.profile
+# Add required version to PATH
+if ! cat $HOME/.profile | grep apache-maven > /dev/null 2>&1; then
+	echo "export PATH=/opt/apache-maven-$mvn_version/bin:$PATH" | tee -a $HOME/.profile > /dev/null 2>&1
 else
-	sed -i "s=apache-maven-[0-9.]*=apache-maven-$mvn_version=" $HOME/.profile
+	sed -i "s=apache-maven-[0-9.]*=apache-maven-$mvn_version=" $HOME/.profile > /dev/null 2>&1
 fi
+
