8472188ddac466b5e8989bfb0aa077ff728a524b
Sasha <sasha7692@gmail.com>
2020-05-05 15:44:44 +0300

    Update mvnset.sh

--- Task1/mvnset.sh
+++ Task1/mvnset.sh
@@ -3,17 +3,29 @@
 set -e
 
 # Variables
-mvn_version=$1 # Example: 3.6.1
 VAR_ERROR=1
 REJECT_INSTALLATION=2
 NOT_SUDO=3
 ANSWER_ERROR=4
 
 # Check parameters
-if [ $# != 1 ]; then
+if [ $# -lt 1 ]; then
 	echo "You forgot to enter maven version"
 	exit $VAR_ERROR
 fi
+
+while [ -n "$1" ]
+do
+case "$1" in
+-y) choice=Y;;
+-v) mvn_version=$2
+shift;;
+esac
+shift
+done
+
+echo $choice
+echo $mvn_version
 
 # Check sudo or root
 if [ "$EUID" -ne 0 ]
@@ -23,12 +35,16 @@ fi
 
 # Сhecking if the required version is installed
 if [ ! -d /opt/apache-maven-"$mvn_version" ]; then	
-	read -p "The version of Maven you requested is not installed. Install? Continue (Y/n)? " -e choice
-	case "$choice" in 
-	  y|Y|"" ) wget -qO- https://archive.apache.org/dist/maven/maven-3/"$mvn_version"/binaries/apache-maven-"$mvn_version"-bin.tar.gz -P /opt | tar -xvz -C /opt > /dev/null 2>&1;;
-	  n|N ) exit $REJECT_INSTALLATION;;
-	  * ) echo "Incorrect answer"; exit $ANSWER_ERROR;;
-	esac
+	if [ "$choice" != "Y" ]; then
+		read -p "The version of Maven you requested is not installed. Install? Continue (Y/n)? " -e choice
+		case "$choice" in 
+		  y|Y|"" ) wget -qO- https://archive.apache.org/dist/maven/maven-3/"$mvn_version"/binaries/apache-maven-"$mvn_version"-bin.tar.gz -P /opt | tar -xvz -C /opt > /dev/null 2>&1;;
+		  n|N ) exit $REJECT_INSTALLATION;;
+		  * ) echo "Incorrect answer"; exit $ANSWER_ERROR;;
+		esac
+	else
+		wget -qO- https://archive.apache.org/dist/maven/maven-3/"$mvn_version"/binaries/apache-maven-"$mvn_version"-bin.tar.gz -P /opt | tar -xvz -C /opt > /dev/null 2>&1
+	fi
 fi
 
 # Add required version to PATH
