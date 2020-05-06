c61b5e502c64dfb3b989a43d9e17f680d3c6d8cd
Sasha <sasha7692@gmail.com>
2020-05-06 14:30:43 +0300

    Update disenable_su_root.sh

--- Task1/disenable_su_root.sh
+++ Task1/disenable_su_root.sh
@@ -6,18 +6,19 @@ set -e
 # Variables
 username=$1
 
-echo "Cmnd_Alias      SUROOT=/bin/su "",\
-/bin/su -,\
-/bin/su - root,\
+echo "Cmnd_Alias SUROOT${username^^}=/bin/su "",\
+/bin/su -, \
+/bin/su - root, \
 /bin/su root,\
-/usr/bin/su "",\
-/usr/bin/su -,\
-/usr/bin/su - root,\
-/usr/bin/su root,\ 
-/bin/bash "",\
-/usr/bin/sudo -s,\
-/usr/bin/sudo su "",\
-/usr/bin/sudo su -,\
-/usr/bin/sudo su - root,\
+/usr/bin/su "", \
+/usr/bin/su -, \
+/usr/bin/su - root, \
+/usr/bin/su root, \
+/bin/bash "", \
+/usr/bin/sudo -s, \
+/usr/bin/sudo su "", \
+/usr/bin/sudo su -, \
+/usr/bin/sudo su - root, \
 /usr/bin/sudo sudo *
-$username ALL = (ALL) ALL, !SUROOT" > /etc/sudoers.d/$username
+
+$username ALL = (ALL) ALL, !SUROOT${username^^}" > /etc/sudoers.d/$username
