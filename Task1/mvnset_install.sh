2fee7dfa63b1af7124ae55476b7b7a004d4feab1
Sasha <sasha7692@gmail.com>
2020-05-06 15:57:23 +0300

    Update mvnset_install.sh

--- Task1/mvnset_install.sh
+++ Task1/mvnset_install.sh
@@ -5,6 +5,7 @@ set -e
 # Exit codes
 SUCCESS=0
 NOT_SUDO=3
+mvnset_version=$1
 
 # Check sudo or root
 if [ "$EUID" -ne 0 ]; then 
@@ -12,14 +13,18 @@ if [ "$EUID" -ne 0 ]; then
 	exit $NOT_SUDO;
 fi
 
+if [ "$mvnset_version" -eq 1 ]; then 
+	$mvnset_version = "";
+fi 
+
 # Download mvnset to /usr/bin/
-wget -q https://raw.githubusercontent.com/Orfey95/MTasks/master/Task1/mvnset -P /usr/bin/
+wget -q https://raw.githubusercontent.com/Orfey95/MTasks/master/Task1/mvnset"$mvnset_version" -P /usr/bin/
 
 # Do mvnset executable
-chmod +x /usr/bin/mvnset
+chmod +x /usr/bin/mvnset"$mvnset_version"
 
 # Report
-if test -f /usr/bin/mvnset; then
+if test -f /usr/bin/mvnset"$mvnset_version"; then
 	echo "Installation was successful"
 else
 	echo "Something went wrong"
